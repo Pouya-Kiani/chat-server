fcac1ff2f260a299cfd0e00c5e6455a6106c1ce9
Pouya Kiani <p.kiani@live.com>
2021-01-05 13:09:23 +0330

    handle CONNECT_WITH_TOKEN and send threads to usrs

--- server/chat-server.js
+++ server/chat-server.js
@@ -4,9 +4,42 @@ const WebSocket = require('ws');
 let models = require('./server.js').models;
 
 const ws = new WebSocket.Server({port: 8085});
-const clients = [];
+let clients = [];
 
 ws.on('connection', (ws) => {
+  function handleError(error) {
+    ws.send(JSON.stringify({
+      type: 'ERROR',
+      error: error,
+    }));
+  };
+  function handleUnexpectedError(errorCode = 0) {
+    ws.send(JSON.stringify({
+      type: 'ERROR',
+      error: {
+        message: 'Unexpected Error, please contact the support',
+        errorCode,
+      },
+    }));
+  }
+  function getInitialThreads(userId) {
+    // Input: user ID as a string.
+    models.Thread.find({where: {
+      users: {like: userId.toString()},
+    }}, (error, threads) => {
+      if (error) {
+        handleError(error);
+      } else if (threads) {
+        console.log(threads);
+        ws.send(JSON.stringify({
+          type: 'INITIAL_THREADS',
+          data: threads,
+        }));
+      } else {
+        handleUnexpectedError();
+      }
+    });
+  };
   function login(email, password) {
     models.User.login({email, password}, (err, result) => {
       if (err) {
@@ -16,7 +49,7 @@ ws.on('connection', (ws) => {
         }));
       } else {
         models.User.findOne(
-          {where: {id: result.userId}, include: 'Profile'},
+          {where: {id: result.userId}},
           (err, user) => {
             if (err) {
               ws.send(JSON.stringify({
@@ -24,13 +57,17 @@ ws.on('connection', (ws) => {
                 error: err,
               }));
             } else {
+              ws.uid = user.id + new Date().getTime().toString();
               const userObject = {
                 id: user.id,
                 email: user.email,
                 ws,
               };
+              // ToDo: User logout function should clean its tail in the server.
+              // This is to prevent multiple client instances in login. Remove when handled properly.
+              clients = clients.filter(C => C.id.toString() !== user.id.toString());
               clients.push(userObject);
-              console.log('current clients: ', clients);
+              console.log('current clients: ', clients, typeof(user.id));
               ws.send(JSON.stringify({
                 type: 'LOGGEDIN',
                 data: {
@@ -38,6 +75,7 @@ ws.on('connection', (ws) => {
                   user: user,
                 },
               }));
+              getInitialThreads(user.id);
             }
           }
         );
@@ -78,6 +116,57 @@ ws.on('connection', (ws) => {
             break;
           case 'LOGIN':
             login(requestJson.data.email, requestJson.data.password);
+            break;
+          case 'CONNECT_WITH_TOKEN':
+            // Check the access token and userId's validity and send user data to client.
+            models.AccessToken.findById(requestJson.data.token, (error, accessToken) => {
+              if (error) {
+                console.log(error.message);
+                ws.send(JSON.stringify({
+                  type: 'ERROR',
+                  message: error.message,
+                }));
+              } else if (accessToken) {
+                if (accessToken.userId.toString() === requestJson.data.userId) {
+                  // Check if user is already logged in.
+                  var userObject = clients.filter(U => (U.id.toString() === accessToken.userId.toString()));
+                  if (userObject.length > 0) {
+                    userObject = userObject[0];
+                  } else {
+                    // If not, create one.
+                    models.User.findById(requestJson.data.userId, (error2, user) => {
+                      if (error2) {
+                        console.log(error.message);
+                        ws.send(JSON.stringify({
+                          type: 'ERROR',
+                          message: error.message,
+                        }));
+                      } else if (user) {
+                        ws.uid = user.id + new Date().getTime().toString();
+                        userObject = {
+                          id: user.id,
+                          email: user.email,
+                          ws,
+                        };
+                        getInitialThreads(user.id);
+                        clients.push(userObject);
+                        console.log('current clients: ', clients);
+                        ws.send(JSON.stringify({
+                          type: 'LOGGEDIN',
+                          data: {
+                            session: accessToken,
+                            user: user,
+                          },
+                        }));
+                      }
+                    });
+                  };
+                };
+              } else {
+                console.log('unidentified error, No user found');
+              }
+            });
+            break;
           case 'SEARCH':
             const keyword = requestJson.data.keyword;
             models.User.find({where: {email: {like: keyword}}}, (error, result) => {
@@ -137,6 +226,8 @@ ws.on('connection', (ws) => {
               }
             });
             break;
+          case 'THREAD_LOAD':
+            break;
           default:
             break;
         }
@@ -144,5 +235,5 @@ ws.on('connection', (ws) => {
     } catch (err) {
       console.log(err);
     };
-  });
+  }); // ws.on('message');
 });
